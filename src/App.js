356a9092d46684ccbc8756787ab1083daf145352
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 21:27:00 +0500

    make dynamic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import useCustomCounter from './Custom';
+import React from 'react'
+function App () {
+  const data = useCustomCounter();
+  return(
+    <React.Fragment>
+    <h1>count up:{data.count}</h1>
+    <button type="button" onClick={data.handleIncrement}>Increment</button>
+    </React.Fragment>
+  )
 }
 
 export default App;
